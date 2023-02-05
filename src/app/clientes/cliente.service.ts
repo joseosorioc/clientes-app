e3c891a701d630dc64f91c5609fe3d02ce62d9ea
Jose Osorio Catalan <jossekarlos10@gmail.com>
2023-02-05 18:00:45 -0500

    update endpoints service cliente.

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -8,7 +8,7 @@ import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ClienteService {
-  private urlEndPoint: string = 'http://localhost:8080/api/clientes';
+  private urlEndPoint: string = 'http://localhost:8080/api';
 
   private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
 
@@ -16,7 +16,7 @@ export class ClienteService {
 
   getClientes(): Observable<Cliente[]> {
     //return of(CLIENTES);
-    return this.http.get(this.urlEndPoint).pipe(
+    return this.http.get( `${this.urlEndPoint}/clientes` ).pipe(
       map(response => response as Cliente[])
     );
   }
@@ -26,15 +26,15 @@ export class ClienteService {
   }
 
   getCliente(id): Observable<Cliente>{
-    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`)
+    return this.http.get<Cliente>(`${this.urlEndPoint}/cliente/${id}`)
   }
 
   update(cliente: Cliente): Observable<Cliente>{
-    return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeaders})
+    return this.http.put<Cliente>(`${this.urlEndPoint}/editar/${cliente.id}`, cliente, {headers: this.httpHeaders})
   }
 
   delete(id: number): Observable<Cliente>{
-    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {headers: this.httpHeaders})
+    return this.http.delete<Cliente>(`${this.urlEndPoint}/eliminar/${id}`, {headers: this.httpHeaders})
   }
 
 }
